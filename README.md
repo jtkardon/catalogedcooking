08fb4f1ec05135dc0e975de39ed679f50f173593
jtkardon <jtkardon@gmail.com>
2023-12-02 17:17:38 -0500

    push

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # catalogedcooking
+https://www.simplyrecipes.com/how-to-make-a-burger-on-the-stove-6890850
---
+++ script.js
@@ -0,0 +1,63 @@
+
+
+function displayRecipe() {
+    var node = document.getElementById('recipe');
+    var div = document.createElement('div');
+    div.setAttribute("class", "grid-item")
+    div.innerHTML = '<a href="test.html"><img id="WPI" src="images/burger.jpg" width="150" /></a> <p>here</p>';
+    node.append(div)
+}
+
+function initBody() {
+
+    for (let i = 0; i < 3; i++) {
+        displayRecipe();
+    }
+}
+
+class Recipe {
+    constructor(name, img, rating, health, difficulty, time, course, ingredients, steps) {
+        this.name = name;
+        this.img = img;
+        this.rating = rating;
+        this.health = health;
+        this.difficulty = difficulty;
+        this.time = time;
+        this.course = course;
+        this.ingredients = ingredients;
+        this.steps = steps;
+    }
+}
+
+
+// function readFiles(dirname, onFileContent, onError) {
+//     const $output = document.getElementById('output')
+//     document.getElementById('file').onchange = function () {
+//         var file = this.files[0];
+
+//         var reader = new FileReader();
+//         reader.onload = function (progressEvent) {
+//             // Entire file
+//             const text = this.result;
+//             $output.innerText = text
+
+//             // By lines
+//             var lines = text.split('\n');
+//             for (var line = 0; line < lines.length; line++) {
+//                 console.log(lines[line]);
+//             }
+//         };
+//         reader.readAsText(file);
+//     };
+
+console.log("here")
+let file = document.getElementById("readfile");
+if (file != null) {
+    file.addEventListener("change", function () {
+        var reader = new FileReader();
+        reader.onload = function (progressEvent) {
+            console.log(this.result);
+        };
+        reader.readAsText(this.files[0]);
+    });
+}
