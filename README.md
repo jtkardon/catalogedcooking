c36b6f7306039f075fd692a157d327de9742a0c7
jtkardon <jtkardon@gmail.com>
2023-12-03 13:51:21 -0500

    c

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # catalogedcooking
 https://www.simplyrecipes.com/how-to-make-a-burger-on-the-stove-6890850
+https://www.foodandwine.com/recipes/caesared-salmon-salad
+https://www.allrecipes.com/recipe/21014/good-old-fashioned-pancakes/
+https://www.allrecipes.com/recipe/8532994/grilled-cheese-roll-ups/
+https://www.allrecipes.com/recipe/11161/chocolate-balls/
---
+++ viewrecipescript.js
@@ -0,0 +1,28 @@
+class Recipe {
+    constructor(name, img, rating, health, difficulty, time, course, ingredients, steps) {
+        this.name = name;
+        this.img = img;
+        this.rating = rating;
+        this.health = health;
+        this.difficulty = difficulty;
+        this.time = time;
+        this.course = course;
+        this.ingredients = ingredients;
+        this.steps = steps;
+    }
+}
+
+
+function initBody() {
+    var currRecipe = new Recipe();
+    var p = document.getElementById('test');
+    var allRecipes = JSON.parse(localStorage["recipes"]);
+
+    allRecipes.forEach(element => {
+        if (element.name === localStorage["currentRecipe"]) {
+            currRecipe = element
+        }
+    });
+    p.innerHTML = JSON.stringify(currRecipe);
+}
+
